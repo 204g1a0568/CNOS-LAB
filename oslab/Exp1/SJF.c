3092aed9c10c454f3e0763f5113f6e397ad45417
204g1a0568 <112165190+204g1a0568@users.noreply.github.com>
2022-09-09 16:14:36 +0530

    SJF.c is created

---
+++ oslab/Exp1/SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: R Nithya
+   Roll No: 204G1A0568
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 09-09-2022
+   Date of Execution: 09-09-2022
+*/
